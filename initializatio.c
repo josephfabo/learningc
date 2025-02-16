99f00e08886ce3956bda866be50cd7b793e7f028
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2025-02-16 15:13:47 +0100

    Update initializatio.c

--- initializatio.c
+++ initializatio.c
@@ -2,8 +2,14 @@
 
 int main()
 {
-  int var = 3;
-  var = 4;
-  printf("%d", var);
+  int var1 = 3;
+  int var2,
+  var2 = var1;
+  printf("%d", var2);
+
+  int two = 2, three= 3, six = 6;
+  int result = (two + three) * six / three;
+  prinf("I am new to programmming but learn to write arithemetic expressions like");
+  prinf("and the result of the expression is: %d", result);
   return();
 }
