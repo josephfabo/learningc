f88367b2c84328a041a82d029e38a1f591adaa64
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2025-02-23 08:58:44 +0100

    Update initializatio.c

--- initializatio.c
+++ initializatio.c
@@ -13,3 +13,90 @@ int main()
   prinf("and the result of the expression is: %d", result);
   return();
 }
+
+#include <stdio.h>
+
+int main() {
+    char dir;
+    scanf("%c", &dir);
+
+    switch (dir) {
+        case 'w':
+            printf("Up");
+            break;
+        case 's':
+            printf("Down");
+            break;
+        case 'a':
+            printf("Left");
+            break;
+        case 'd':
+            printf("Right");
+            break;
+        default:
+            printf("Wrong");
+    }
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+	int i = 0;
+    int stars;
+    
+    scanf("%d", &stars);
+    
+    do {
+        printf("*");
+        i++;
+    } while (i < stars);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+	int start, end;
+	scanf("%d", &start);
+	scanf("%d", &end);
+	
+	int i = start;
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= end);
+
+    printf("\n"); 
+    
+    return 0;
+}
+
+int main() {
+	int i;
+	
+    float t[] = {10.3, 15.4, 21.8, 22.5, 25.1, 27.4, 28.8, 27.9, 20.1, 22, 19.4, 14.2};
+
+	scanf("%d", &i);
+
+    printf("%f", t[i]);
+    
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    float results[] = {0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1};
+    
+    int total = 0;
+
+    for(int i=0;i<28;i++) {
+        total += results[i];
+    }
+    printf("%d", total);
+    
+    return 0;
+}
